3da945a6e3793a795039863571cac8fdf1551bc9
Shubham-Kumar226 <shubhamkumardron@gmail.com>
2026-04-02 18:36:19 +0530

    Completed my module

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-# app
+from flask import Flask
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    conn = sqlite3.connect("logs.db")
+    data = conn.execute("SELECT * FROM logs").fetchall()
+
+    result = ""
+    for row in data:
+        result += f"{row}<br>"
+
+    return result
+
+if __name__ == "__main__":
+    app.run(debug=True)
