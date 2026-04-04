3c6a9a45951a49d468f9b212a253dd1d0c7fa4b1
pankaj <pankajbhatiya869@gmail.com>
2026-04-04 16:55:45 +0530

    Added my module files

--- db.py
+++ db.py
@@ -1,18 +1,21 @@
 import sqlite3
 
-# connect to database
-conn = sqlite3.connect("logs.db")
-cursor = conn.cursor()
-
-# create table
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS logs (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    syscall TEXT,
-    status TEXT
-)
-""")
+def init_db():
+    conn = sqlite3.connect("logs.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS logs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        syscall TEXT,
+        status TEXT
+    )
+    """)
+    conn.commit()
+    conn.close()
 
 def log_data(syscall, status):
+    conn = sqlite3.connect("logs.db")
+    cursor = conn.cursor()
     cursor.execute("INSERT INTO logs (syscall, status) VALUES (?, ?)", (syscall, status))
     conn.commit()
+    conn.close()
